c6cf2b4cdbfaffb3736e69dbdc945170af46bef8
WenTian1111 <161295518+WenTian1111@users.noreply.github.com>
2025-09-14 13:34:07 +0000

    Deploying to gh-pages from @ WenTian1111/WenTian1111.github.io@a8bfb967860395e1546c37f45d94db123a8196d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-certificates-amp-honors",
+          title: "Certificates &amp; Honors",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/certificates/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
