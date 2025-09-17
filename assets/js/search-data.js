0115ffb3a8010ec0c05a1b5dc1465bdd494d7b81
WenTian1111 <161295518+WenTian1111@users.noreply.github.com>
2025-09-17 03:13:04 +0000

    Deploying to gh-pages from @ WenTian1111/WenTian1111.github.io@23f26657aec7c86e03faa47b3e6886d5d9203245 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "An overview of my key research and academic projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
